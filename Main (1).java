a05c07ef4a32396f835a6173f0307c9630a5cb69
alladisriya <51986170+alladisriya@users.noreply.github.com>
2020-06-18 14:09:24 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		while(true)
+		{
+			int n=sc.nextInt();
+			if(n==42)
+			break;
+			else
+			System.out.println(n);
+		}
+	}
+}
